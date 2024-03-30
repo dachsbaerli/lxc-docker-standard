feb9ee422dd1617a4655a762105104602f71d7a5
dachsbaerli <m.mayorga.vergara@gmail.com>
2024-03-30 20:46:34 +0100

    script extended with recommendation to reboot and reboot option

--- start.sh
+++ start.sh
@@ -1,17 +1,25 @@
 #! /bin/sh
-apt-get update && apt-get upgrade -y && apt autoremove \
-&& apt-get install curl -y \
-&& curl -sSL https://get.docker.com | sh \
-&& apt-get install docker-compose -y \
-&& timedatectl set-timezone Europe/Berlin
+echo "install updates"
+apt-get update && apt-get upgrade -y && apt autoremove
+
+echo "Install curl"
+apt-get install curl -y
+
+echo
+echo "install docker and docker-compose"
+curl -sSL https://get.docker.com | sh
+apt-get install docker-compose -y
+
+echo
+echo "set timezone to Europe/Berlin"
+timedatectl set-timezone Europe/Berlin
+
 echo ""
 echo "---packages up-to-date---"
 echo "---curl installed---"
 echo "---docker installed---"
 echo ""
 echo "Docker installed - Reboot required"
-
-
 while true; do
     read -r -p "Do you wish to reboot the system? (Y/N): " answer
     case $answer in
