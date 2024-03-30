2b9e0429ec6aeba7071066ecc1a26be109f7b0fe
dachsbaerli <165464290+dachsbaerli@users.noreply.github.com>
2024-03-30 10:14:36 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
+#! /bin/sh
 apt-get update && apt-get upgrade -y && apt autoremove
-apt install curl
+apt install curl -y
 curl -sSL https://get.docker.com | sh
 apt-get install docker-compose
+echo "packages up-to-date"
+echo "curl installed"
+echo "docker installed
