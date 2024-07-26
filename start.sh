f36861b741036efe761614072cd4f2fe4374c209
dachsbaerli <165464290+dachsbaerli@users.noreply.github.com>
2024-07-26 23:21:28 +0200

    Update start.sh
    changed installation routine for docker to be able to upgrade docker via apt

--- start.sh
+++ start.sh
@@ -2,20 +2,38 @@
 echo "install updates"
 apt-get update && apt-get upgrade -y && apt autoremove
 
-echo "install curl"
-apt-get install curl -y
-
-echo
-echo "Install docker"
-curl -sSL https://get.docker.com | sh
-# apt-get install docker-compose -y
 
 echo
 echo "set timezone to Europe/Berlin"
 timedatectl set-timezone Europe/Berlin
 
+
+echo "install curl"
+apt-get install curl -y
+
+
+echo
+echo "Install docker"
+#curl -sSL https://get.docker.com | sh
+# apt-get install docker-compose -y
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
 echo ""
 echo "---packages up-to-date---"
+echo "---timezone set---"
 echo "---curl installed---"
 echo "---docker installed---"
 echo ""
